bd4ee602484e5d6cc1b377dfd058ef49fabfa883
Oleg Sivokon <olegsivokon@gmail.com>
2018-04-18 23:19:58 +0300

    Try to discover APR includes

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3.6
 # -*- coding: utf-8 -*-
 
+import os
 # from distutils.core import setup, Extension
 from setuptools import setup, Extension
 
 # './include/apr/unix/.libs/libapr-1.so'
+
+apr_lib_candidates = (
+    '/usr/include/apr-1.0',
+    '/usr/include/apr-1',
+)
+
+apr_lib = None
+
+for lib in apr_lib_candidates:
+    if os.path.isdir(lib):
+        apr_lib = lib
+        break
+
+if not apr_lib:
+    raise Exception('Cannot find Apache Portable Runtime headers')
 
 setup(
     packages=['protopy'],
@@ -33,7 +49,7 @@ setup(
                 'protopy/lib/list.c',
                 'protopy/wrapper.c',
             ],
-            include_dirs=['/usr/include/apr-1.0'],
+            include_dirs=[apr_lib],
             libraries=['apr-1'],
             # include_dirs=['./include/apr/unix/include']
             # extra_link_args=['-Wl,-rpath,{}'.format(LIB_DIR)]
