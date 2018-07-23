156e7251d69bc1426075a540b0f60d46bb436e93
Oleg Sivokon <olegs@traiana.com>
2018-07-23 10:38:56 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ setup(
     },
     packages=['protopy'],
     name='protopy',
-    version='0.0.2',
+    version='0.0.3',
     description='Parser for Google Protobuf',
     author='Oleg Sivokon',
     author_email='olegs@traiana.com',
