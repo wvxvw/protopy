4f03fcc96500732b70e3ecec99109b9a5faadb33
Oleg Sivokon <olegs@traiana.com>
2018-08-23 16:19:37 +0300

    Added comparison to Google's Protobuf, we are almost 10 times slower

--- test/performance/protopy_performance/runner.py
+++ test/performance/protopy_performance/runner.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
 from os import path
 from time import time
+from importlib import import_module
 
 from protopy.parser import BinParser
 
 
 class Runner:
 
-    def __init__(self, idl, bins, times=100, threads=1):
+    def __init__(self, idl, pyidl, bins, times=100, threads=1):
         self.idl = idl
+        self.pyidl = pyidl
         self.bins = bins
         self.threads = threads
         self.times = times
 
         self.protopy_parser = BinParser([path.dirname(self.idl)])
+        self.protopy_parser.def_parser.parse(path.basename(self.idl))
+        sys.path.insert(0, path.dirname(self.pyidl))
+        self.pdef = import_module(
+            path.splitext(path.basename(self.pyidl))[0]
+        ).AllMessages
 
     def run_protopy(self):
         start, step = time(), 0
@@ -39,6 +48,31 @@ class Runner:
                     start = step + start
 
         print('Protopy:')
+        print('========')
+        print('Bytes processed: {}'.format(size))
+        print('I/O time: {}'.format(io_time))
+        print('Parse time: {}'.format(parse_time))
+
+    def run_protobuf(self):
+        start, step = time(), 0
+        io_time, parse_time = 0, 0
+        size = 0
+
+        for _ in range(self.times):
+            for b in self.bins:
+                with open(b, 'rb') as payload:
+                    r = payload.read()
+                    step = time() - start
+                    io_time += step
+                    start = step + start
+                    size += len(r)
+                    d = self.pdef()
+                    d.ParseFromString(r)
+                    step = time() - start
+                    parse_time += step
+                    start = step + start
+
+        print('Google Protobuf:')
         print('========')
         print('Bytes processed: {}'.format(size))
         print('I/O time: {}'.format(io_time))
